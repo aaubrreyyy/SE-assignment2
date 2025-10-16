24edf02d16b182f8924767b6c47d8b60848e6364
aaubrreyyy <amlister06@gmail.com>
2025-10-16 12:54:10 -0500

    Altered a specific line!

--- SE-assignment 2 - Hello World File.py
+++ SE-assignment 2 - Hello World File.py
@@ -2,4 +2,4 @@ import os
 import sys
 print("\nHello World :))))" + "\nThis is Aubrey!!")
 print('\nI already added my name in the first step, lol. I will add a scripture I like instead: \n*** "Blesses are the merciful, for they obtain mercy."" -Matthew 5:7(NKJV) ***\n')
-print("Addinf something else to see why Git isn't showing a difference...")
+print("Adding something else. :)")
