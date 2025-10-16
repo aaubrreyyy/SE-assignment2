fe51681f19eff90351e59993353a1ed68563f448
aaubrreyyy <amlister06@gmail.com>
2025-10-16 15:39:35 -0500

    Added in my additional line to resolve issue 1

--- SE-assignment 2 - Hello World File.py
+++ SE-assignment 2 - Hello World File.py
@@ -3,3 +3,4 @@ import sys
 print("\nHello World :))))" + "\nThis is Aubrey!!")
 print('\nI already added my name in the first step, lol. I will add a scripture I like instead: \n*** "Blesses are the merciful, for they obtain mercy."" -Matthew 5:7(NKJV) ***\n')
 print("Addinf something else to see why Git isn't showing a difference...")
+print("Adding a random line here!! to solve issue 1.")
